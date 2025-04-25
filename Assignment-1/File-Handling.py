98ac1903ba9ac7f4d27fee1ca6643959391bb255
RohitTiwari2004 <rohittiwaritit2004@gmail.com>
2025-04-25 21:52:24 +0545

    fourth commit

--- Assignment-1/File-Handling.py
+++ Assignment-1/File-Handling.py
@@ -5,3 +5,30 @@
 #Reading from a file
 #Writing to a file
 #Closing the file
+
+
+#write to a file
+file = open('example2.txt', 'w')
+#it creates a file if it is does not exist on folder.
+file.write('Its been a long day')
+file.close()
+
+#append mode
+file = open('example2.txt', 'a') #append mode
+file.write('\nYeah!, it is')
+file.close()
+
+file = open('ShortInfo.txt', 'w')
+file.write('Its me Rohit Tiwari')
+file.close()
+
+#to add some sentence on ShortInfo.txt, I use append mode
+file = open('ShortInfo.txt', 'a') #add incremental value..  
+file.write('\nAlways curious to learn new stuffs')
+file.close()
+
+#close a file using with statement
+with open('ShortInfo.txt', 'r') as file:
+ message = file.readlines()
+ print(message)
+
